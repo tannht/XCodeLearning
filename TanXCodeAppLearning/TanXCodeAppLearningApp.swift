c4a948b4b90ef5848066f85d411df3c6d5705aa6
Tan Nguyen <tannguyen.jp@interspace.vn>
2025-01-05 10:40:30 +0700

    Initial Commit

---
+++ TanXCodeAppLearning/TanXCodeAppLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanXCodeAppLearningApp.swift
+//  TanXCodeAppLearning
+//
+//  Created by Tan on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TanXCodeAppLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
